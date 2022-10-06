6314dbdfb0ddabf077b30f4a8262fc5c0daf271d
alfredochs <chanta.alfredo@gmail.com>
2022-10-06 14:55:56 +0200

    works finally

--- business-logic/models/Player.js
+++ business-logic/models/Player.js
@@ -23,34 +23,28 @@ module.exports = (sequelize) => {
         name: {
             type: Sequelize.CHAR(50),
             allowNull: false,
-            // after: 'id'
         },
         surname: {
             type: Sequelize.CHAR(50),
             allowNull: true,
-            // after: 'name'
         },
         nick_name: {
             type: Sequelize.CHAR(50),
             allowNull: true,
-            // after: 'surname',
             unique: true,
         },
         birth_date: {
             type: Sequelize.DATEONLY,
             allowNull: true,
-            // after: 'nick_name'
         },
         goalkeeper_provisory: {
             type: Sequelize.BOOLEAN,
             allowNull: false,
             defaultValue: false,
-            // after: 'birth_date'
         },
         level_id: {
             type: Sequelize.INTEGER,
             allowNull: false,
-            // after: 'goalkeeper_provisory',
             references: {
                 model: 'levels',
                 key: 'id',
@@ -59,7 +53,6 @@ module.exports = (sequelize) => {
         role_id: {
             type: Sequelize.INTEGER,
             allowNull: false,
-            // after: 'level_id',
             references: {
                 model: 'roles',
                 key: 'id',
@@ -68,7 +61,6 @@ module.exports = (sequelize) => {
         language_id: {
             type: Sequelize.CHAR(2),
             allowNull: false,
-            // after: 'role_id',
             references: {
                 model: 'languages',
                 key: 'id',
@@ -78,7 +70,6 @@ module.exports = (sequelize) => {
             type: Sequelize.BOOLEAN,
             allowNull: false,
             defaultValue: false,
-            // after: 'language_id'
         },
         created: {
             type: Sequelize.DATE,
