180a8599d17afc7a4bc0af5832950ba94826e3ab
luca quacquarelli <luca.quacquarelli@conduent.com>
2022-10-18 14:12:21 +0200

    card style test

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <ul class="nav justify-content-center  ">
+    <nav class="nav justify-content-center">
         <router-link class="nav-item" to="/">
             <a class="nav-link" aria-current="page">Home</a>
         </router-link>
@@ -7,7 +7,7 @@
         <router-link class="nav-item" to="players">
             <a class="nav-link" aria-current="page">Players</a>
         </router-link>
-    </ul>
+    </nav>
     <div class="container">
         <router-view />
     </div>
--- src/components/PlayerCard.vue
+++ src/components/PlayerCard.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="card">
-        <div class="card-header p-3 bg-info">
-
+        <div class="card-header d-flex justify-content-between align-items-start position-relative p-3">
+            <div class="round round-left p-4 rounded-circle bg-white"></div>
+            <div class="round round-right p-4 rounded-circle bg-white"></div>
         </div>
-        <div class="card-body d-flex flex-wrap px-0">
+        <div class="card-body d-flex flex-wrap p-0">
             <div class="col-4 text-center">
                 <h2 class="fw-bold">
                     {{ level.percentage }}
@@ -34,7 +35,7 @@
                 </span>
             </div>
         </div>
-        <div class="card-footer bg-info">
+        <div class="card-footer p-3">
 
         </div>
     </div>
@@ -52,7 +53,52 @@ export default {
 
 <style lang="scss" scoped>
     .card{
-    
+        border: 0 !important;
+
+        .card-header {  
+            background-color: #d8ad48 !important;
+            border: 0 !important;
+            overflow: hidden;
+            border-radius: 150vh 150vh 0% 0%/40vh 40vh 0% 0%  !important;
+            -moz-border-radius: 150vh 150vh 0% 0%/40vh 40vh 0% 0%  !important;
+            -webkit-border-radius: 150vh 150vh 0% 0%/40vh 40vh 0% 0%  !important;
+
+            .round {
+                position: absolute;
+            }
+
+            .round-left {
+                top: -85%;
+                left: -15%;
+            }
+            .round-right {
+                top: -85%;
+                right: -15%;
+            }
+        }
+        .card-footer {  
+            background-color: #d8ad48 !important;
+            border: 0 !important;
+            overflow: hidden;
+            border-radius: 0% 0% 150vh 210vh/0% 0% 40vh 40vh !important;
+            -moz-border-radius: 0% 0% 150vh 210vh/0% 0% 40vh 40vh !important;
+            -webkit-border-radius: 0% 0% 150vh 210vh/0% 0% 40vh 40vh !important;
+ 
+            &:after {
+                content: "";
+                width: 20px;
+                height: 20px;
+                transform: rotate(-45deg); background-color: #d8ad48 !important;
+                position: absolute;
+                bottom: -10px;
+                left: calc(50% - 10px);
+            }
+        }
+
+        .card-body {
+            background-color: #d8ad48 !important;
+        }
+
         img {
             position: relative;
             top: 15%;
