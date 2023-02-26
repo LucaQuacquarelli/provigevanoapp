906cca423f6042a1c158153c1a945adce2172116
alfredochs <chanta.alfredo@gmail.com>
2023-02-26 01:02:08 +0100

    try while

--- src/views/TeamsView.vue
+++ src/views/TeamsView.vue
@@ -6,17 +6,18 @@
 </template>
 
 <script>
-import { mapState } from "vuex"
+import { mapState } from "vuex";
 export default {
     name: 'TeamsView',
     data() {
         return {
+            lastResult: null
             // sumAvailablesAndGoalkeepers: this.all_players_availables.length + this.all_goal_keepers.length,
             // numberOfTeams : this.possibility.teams,
             // numberOfPlayersForTeam : this.possibility.players
-        }
+        };
     },
-    computed:{
+    computed: {
         ...mapState([
             'all_players_availables',
             'all_goal_keepers',
@@ -26,91 +27,105 @@ export default {
     methods: {
         setTeamsByGoalkeepers() {
             return new Promise((resolve) => {
-                const allTeams = []
-                const all_goal_keepers = this.all_players_availables.filter( player => {
-                    return  player.role.name == 'goalkeeper'
-                })
+                const allTeams = [];
+                const all_goal_keepers = this.all_players_availables.filter(player => {
+                    return player.role.name == 'goalkeeper';
+                });
                 all_goal_keepers.forEach(
                     gk => {
-                        var team = []
+                        var team = [];
                         if (!team.includes(gk)) {
-                            team.push(gk)
+                            team.push(gk);
                         }
-                        allTeams.push(team)
+                        allTeams.push(team);
                     }
-                    )
-                resolve(allTeams)
-            })
+                );
+                resolve(allTeams);
+            });
         },
-        setPlayersForTeams(allTeams){
+        setPlayersForTeams(allTeams) {
             return new Promise((resolve) => {
                 const oldAvailables = this.all_players_availables.filter(player => {
                     return player.role.name == 'player';
-                })
-                oldAvailables.sort(()=> Math.random() - 0.5)
-                const teamLength = oldAvailables.length/this.possibility.teams
+                });
+                oldAvailables.sort(() => Math.random() - 0.5);
+                const teamLength = oldAvailables.length / this.possibility.teams;
                 allTeams.forEach(
                     team => {
-                        var playersForTeam = oldAvailables.slice(0, teamLength)
-                        team.push(...playersForTeam)
-                        oldAvailables.splice(0, teamLength)
+                        var playersForTeam = oldAvailables.slice(0, teamLength);
+                        team.push(...playersForTeam);
+                        oldAvailables.splice(0, teamLength);
                     }
-                )
-                resolve(allTeams)
-            })
+                );
+                resolve(allTeams);
+            });
         },
         getSumPlayersPercentage(allTeamsSorted) {
             return new Promise((resolve) => {
-                let sum = 0
-                let averages = []
+                let sum = 0;
+                let averages = [];
                 this.all_players_availables.forEach(player => {
-                    sum += player.level.percentage
-                })
+                    sum += player.level.percentage;
+                });
 
-                allTeamsSorted.forEach( team => {
-                    let average = 0
-                    team.forEach(player=>{
-                        average += player.level.percentage
-                    })
-                    averages.push(average)
-                })
+                allTeamsSorted.forEach(team => {
+                    let average = 0;
+                    team.forEach(player => {
+                        average += player.level.percentage;
+                    });
+                    averages.push(average);
+                });
 
                 resolve({
                     allTeamsSorted,
                     sum,
                     averages
-                })
-            })
+                });
+            });
         },
-        // setAgain() {
-        //     this.setTeamsByGoalkeepers().then((allTeams) => {
-        //         this.setPlayersForTeams(allTeams).then((allTeamsSorted) => {
-        //             this.getSumPlayersPercentage(allTeamsSorted).then((results) => {
-        //             console.log("🚀 ~ file: TeamsView.vue ~ line 88 ~ this.getSumPlayersPercentage ~ results", results)
-        //             })
-        //         })
-        //     })
-        // }
+        setFinalTeams() {
+            return new Promise((resolve) => {
+                this.setTeamsByGoalkeepers().then((allTeams) => {
+                    this.setPlayersForTeams(allTeams).then((allTeamsSorted) => {
+                        this.getSumPlayersPercentage(allTeamsSorted).then((results) => {
+                            const { sum, averages, allTeamsSorted } = results;
+                            console.log("🚀 ~ file: TeamsView.vue:92 ~ this.getSumPlayersPercentage ~ averages:", averages);
+                            const avgForTeam = Math.round(sum / allTeamsSorted.length);
+                            let areEqual = averages.every(average => average == avgForTeam);
+                            resolve(areEqual);
+                        });
+                    });
+                });
+            });
+        }
     },
     created() {
         if (this.$store.state.possibility == null || Object.keys(this.all_players_availables).length === 0) {
-            this.$router.replace('/choose_players')
+            this.$router.replace('/choose_players');
         } else {
-            this.setTeamsByGoalkeepers().then((allTeams) => {
-                this.setPlayersForTeams(allTeams).then((allTeamsSorted) => {
-                    this.getSumPlayersPercentage(allTeamsSorted).then((results) => {
-                    console.log("🚀 ~ file: TeamsView.vue ~ line 102 ~ this.getSumPlayersPercentage ~ results", results.averages)
-                    const avgForTeam = Math.round(results.sum/this.possibility.teams)
-                    const equal = results.averages.some(average => average > avgForTeam)
-                    console.log("🚀 ~ file: TeamsView.vue ~ line 104 ~ this.getSumPlayersPercentage ~ equal", equal ? 'non sono equilibrate' : 'sono equilibrate')
-                    })
-                })
-            })
+            this.setFinalTeams().then((result) => {
+                this.lastResult = result;
+                if (this.lastResult == false) {
+                    console.log("🚀 ~ file: TeamsView.vue:109 ~ this.setFinalTeams ~ this.lastResult:", this.lastResult);
+                    let counter = 0;
+                    while (this.lastResult == false) {
+                        counter++;
+                        console.log("🚀 ~ file: TeamsView.vue:113 ~ this.setFinalTeams ~ counter:", counter);
+                        this.setFinalTeams().then((result) => {
+                            if (result == true) {
+                                this.lastResult = result;
+                                console.log("🚀 ~ file: TeamsView.vue:117 ~ this.setFinalTeams ~ this.lastResult:", this.lastResult);
+                                return;
+                            }
+                        });
+                    }
+                }
+            });
+            console.log("🚀 ~ file: TeamsView.vue:117 ~ this.setFinalTeams ~ result:", this.lastResult);
+
         }
     },
-}
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
